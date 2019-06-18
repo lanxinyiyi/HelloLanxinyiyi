d77517c2aa7942bd0dd622ebd526694192621d4a
zhangtianfu <zhangtianfu@bytedance.com>
2019-06-18 17:13:27 +0800

    Init

---
+++ HelloLanxinyiyi/Classes/Common.h
@@ -0,0 +1,20 @@
+//
+//  Common.h
+//  HelloZTF
+//
+//  Created by zhangtianfu on 2019/6/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Common : NSObject
+
++ (NSInteger)add:(NSInteger)a with:(NSInteger)b;
+
++ (NSInteger)reduce:(NSInteger)a with:(NSInteger)b;
+
+@end
+
+NS_ASSUME_NONNULL_END
